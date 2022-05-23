969de76a20acf61518c00f885953c66a47a48038
monira-jahan-tania <tania598330@gmail.com>
2022-05-24 01:59:48 +0600

    reviews

---
+++ src/Pages/Home/Reviews.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import people1 from '../../images/people1.png';
+import people2 from '../../images/people2.png';
+import people3 from '../../images/people3.png';
+import Review from './Review';
+import quote from '../../images/quote.svg';
+
+const Reviews = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: '',
+            location: 'california',
+            img: people1
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: '',
+            location: 'california',
+            img: people2
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: '',
+            location: 'california',
+            img: people3
+        },
+    ];
+    return (
+        <section className='my-28'>
+            <div className='my-10'>
+                <div>
+                    <h4 className="text-5xl text-primary font-bold text-center">Reviews</h4>
+                    <h2 className='text-3xl text-center'>What our Clients say</h2>
+                </div>
+
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Reviews;
