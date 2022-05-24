97cc7a35b10c602d0a50dc274f89d5e024942757
monira-jahan-tania <tania598330@gmail.com>
2022-05-25 02:57:26 +0600

    require auth done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Login/Register';
 import Purchase from './Pages/Purchase/Purchase';
+import RequireAuth from './Pages/Login/RequireAuth';
 
 function App() {
   return (
@@ -18,7 +19,12 @@ function App() {
         <Route path='/login' element={<Login />}>
         </Route>
         <Route path='/register' element={<Register />}></Route>
-        <Route path='/purchase/:id' element={<Purchase />}></Route>
+        <Route path='/purchase/:id' element={
+          <RequireAuth>
+            <Purchase />
+          </RequireAuth>
+        }></Route>
+
 
 
       </Routes>
